63b4c81823aa58e0d37e34e865dbc3e1bd89cf7c
otischung <my072814638@gmail.com>
2023-11-07 15:49:04 +0800

    remove unneed

--- install/system_interfaces/share/system_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(system_interfaces_IDL_FILES "msg/NodeChange.idl;msg/CheckNode.idl")
-set(system_interfaces_INTERFACE_FILES "msg/NodeChange.msg;msg/CheckNode.msg")

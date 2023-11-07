756488eeda22dffd3583ba0c19e64d95ec546194
otischung <my072814638@gmail.com>
2023-11-07 15:48:16 +0800

    modify setup.py

--- src/template/setup.py
+++ src/template/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "robot_node = template.template:main" 
+            "centerNode = template.template:main"
         ],
     },
 )
